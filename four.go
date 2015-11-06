94f0fb470d0f050ca9563dedb40217e7456c0c4d
Gabriel Dalay <gdalay@jibe.com>
2015-11-05 19:16:05 -0500

    Added solution to problem 4

---
+++ four.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	fmt.Println("This number is the largest palindrome: ", findPalindrome(999, 900))
+}
+
+func findPalindrome(high, low int) int {
+	var palindrome int
+	for i := high; i > low; i-- {
+		for j := high; j > low; j-- {
+			result := i * j
+			resultStr := strconv.Itoa(result)
+			if resultStr == reverse(resultStr) && result > palindrome {
+				palindrome = result
+			}
+		}
+	}
+	return palindrome
+}
+
+func reverse(s string) string {
+	strInRunes := []rune(s)
+	for i, j := 0, len(strInRunes)-1; i < j; i, j = i+1, j-1 {
+		strInRunes[i], strInRunes[j] = strInRunes[j], strInRunes[i]
+	}
+	return string(strInRunes)
+}
