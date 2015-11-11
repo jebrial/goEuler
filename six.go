5c515f1b385c9c6f6a1288da892996529b77e5ee
Gabriel Dalay <gdalay@jibe.com>
2015-11-11 16:46:09 -0500

    Added solution for problem 6

---
+++ six.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(squareOfSum(10) - sumOfSquare(10))
+	fmt.Println(squareOfSum(100) - sumOfSquare(100))
+}
+
+func sumOfSquare(num int) int {
+	result := 0
+	for i := 1; i <= num; i++ {
+		result += square(i)
+	}
+	return result
+}
+
+func squareOfSum(num int) int {
+	result := 0
+	for i := 1; i <= num; i++ {
+		result += i
+	}
+	return square(result)
+}
+
+func square(num int) int {
+	return num * num
+}
